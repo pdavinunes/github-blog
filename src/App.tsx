7f6cafd689f7810995fd6ea1fbe5ddd2150af6d0
pdavinunes <p.davi.nunes@gmail.com>
2023-01-22 13:17:07 -0300

    first commit 🎉

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
